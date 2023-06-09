500e92797667122b8bf55594a7ec3266f8cacd4d
Camila <cf.silv466@gmail.com>
2023-06-09 14:47:04 -0300

    Projeto gerador de senha finalizado.

---
+++ gerador-de-senha/src/App.jsx
@@ -0,0 +1,14 @@
+import MainContent from "./components/MainContent/MainContent";
+import MainFooter from "./components/MainFooter/MainFooter";
+
+export default function App() {
+  
+  return (
+    <>
+      <MainContent />
+      <MainFooter />
+    </>
+  )
+}
+
+
---
+++ gerador-de-senha/src/components/MainFooter/MainFooter.jsx
@@ -0,0 +1,9 @@
+export default function MainFooter() {
+    return (
+        <footer className="main-footer"> 
+            <span className="main-footer__label">
+            2023 &copy; Desenvolvido por Camila Silva 
+            </span>
+        </footer>
+    )
+}
