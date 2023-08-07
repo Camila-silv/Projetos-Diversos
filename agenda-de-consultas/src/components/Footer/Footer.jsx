94f13f30461039b6146802dc21c8d14487d00093
Camila <cf.silv466@gmail.com>
2023-08-07 11:55:40 -0300

    Container floating adicionado

--- agenda-de-consultas/src/components/Footer/Footer.jsx
+++ agenda-de-consultas/src/components/Footer/Footer.jsx
@@ -1,30 +1,64 @@
-import {FiCalendar, FiUsers } from "react-icons/fi";
-import {AiOutlineBarChart} from "react-icons/ai";
-import {HiMenu} from "react-icons/hi";
-
+import { FiCalendar, FiUsers } from "react-icons/fi";
+import { AiOutlineBarChart } from "react-icons/ai";
+import { HiMenu } from "react-icons/hi";
+import { TbMapPin } from "react-icons/tb";
+import { BsCameraVideo } from "react-icons/bs";
+import { useState } from "react";
+import ContainerFloating from "../Container-floating/ContainerFloating";
 
 export default function Footer() {
+  const [layout, setLayout] = useState(false);
+
   return (
     <footer className="main-footer">
-      <button className="main-footer__floating-button">+</button>
+      {layout === true ? (
+        <div className="main-footer__container-floating-buttons">
+          <ContainerFloating label="Nova consulta na casa do paciente">
+            <TbMapPin />
+          </ContainerFloating>
+
+          <ContainerFloating label="Nova consulta por vídeo">
+            <BsCameraVideo />
+          </ContainerFloating>
+
+          <button
+            className="container-floating-buttons__floating-button button-floating-button"
+            onClick={() => {
+              setLayout(!layout);
+            }}
+          >
+            +
+          </button>
+        </div>
+      ) : (
+        <button
+          className="container-floating-buttons__floating-button button-floating-button"
+          onClick={() => {
+            setLayout(!layout);
+          }}
+        >
+          +
+        </button>
+      )}
+
       <nav className="main-footer__nav-footer">
         <a href="/" className="nav-footer__links-nav-footer">
-          <FiCalendar className="links-nav-footer__icon"/>
+          <FiCalendar className="links-nav-footer__icon" />
           <span>Consultas</span>
         </a>
 
         <a href="/" className="nav-footer__links-nav-footer">
-          <FiUsers className="links-nav-footer__icon"/>
+          <FiUsers className="links-nav-footer__icon" />
           <span>Clientes</span>
         </a>
 
         <a href="/" className="nav-footer__links-nav-footer">
-          <AiOutlineBarChart className="links-nav-footer__icon"/>
+          <AiOutlineBarChart className="links-nav-footer__icon" />
           <span>Relatórios</span>
         </a>
 
         <a href="/" className="nav-footer__links-nav-footer">
-          <HiMenu className="links-nav-footer__icon"/>
+          <HiMenu className="links-nav-footer__icon" />
           <span>Menu</span>
         </a>
       </nav>
