d862cbe80b8ad386b14e2ab29ef4eb46d1db7b74
Camila <cf.silv466@gmail.com>
2023-08-08 15:57:11 -0300

    Código refaturado.

--- agenda-de-consultas/src/components/Container-modal/Container-modal.jsx
+++ agenda-de-consultas/src/components/Container-modal/Container-modal.jsx
@@ -25,7 +25,7 @@ export default function ContainerModal() {
             setOpenForm(!openForm);
           }}
         >
-          Abrir modal
+          Nova consulta
         </button>
       )}
     </div>
