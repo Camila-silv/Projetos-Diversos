6a6d5f89932c8a9e3a354c2d8f685b537eee25cd
Camila <cf.silv466@gmail.com>
2023-10-18 16:13:15 -0300

    Bold adicionado
    Bold adicionado ao elemento faq-item ao ser clicado.

--- faq-accordion-card_novato/faq-accordion-card_novato/app/index.js
+++ faq-accordion-card_novato/faq-accordion-card_novato/app/index.js
@@ -1,28 +1,37 @@
 const faqListItem = document.querySelectorAll(".faq-list__faq-list-item");
 const buttons = document.querySelectorAll(".faq-item__button");
-
+const questions = document.querySelectorAll(".faq-list-item__faq-item");
 const accordion = document.querySelectorAll(".faq-list-item__accordion");
 
 faqListItem.forEach((faqItem) => {
   const paragraphy = faqItem.children[2];
 
+  
   faqItem.addEventListener("click", () => {
+    questions.forEach(item => {
+      item.classList.remove("faq-list-item__faq-item--stronge");
+    })
     accordion.forEach((item) => {
       item.classList.add("faq-list-item__accordion--hidden");
+      
     });
+    faqItem.children[0].classList.add("faq-list-item__faq-item--stronge");
     paragraphy.classList.remove("faq-list-item__accordion--hidden");
+   
   });
-
 });
 
 buttons.forEach((faqItem) => {
   const paragraphy = faqItem.parentNode.children[2];
 
   faqItem.addEventListener("click", () => {
+    questions.forEach(item => {
+      item.classList.remove("faq-list-item__faq-item--stronge");
+    })
     accordion.forEach((item) => {
       item.classList.add("faq-list-item__accordion--hidden");
     });
+    faqItem.parentNode.children[0].classList.add("faq-list-item__faq-item--stronge");
     paragraphy.classList.remove("faq-list-item__accordion--hidden");
   });
 });
-
