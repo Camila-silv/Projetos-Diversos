7752c405ae6a0d28b6463dcf799a4306dcb9804a
Camila <cf.silv466@gmail.com>
2024-03-15 19:25:42 -0300

    Delete gerador-de-senha directory

--- gerador-de-senha/src/components/MainFooter/MainFooter.jsx
+++
@@ -1,9 +0,0 @@
-export default function MainFooter() {
-    return (
-        <footer className="main-footer"> 
-            <span className="main-footer__label">
-            2023 &copy; Desenvolvido por Camila Silva 
-            </span>
-        </footer>
-    )
-}
